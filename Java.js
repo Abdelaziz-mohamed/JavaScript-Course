1e2c96f9565dc23a6592a5713615dbf784438e74
miro <kosaveski@gmail.com>
2020-09-05 14:06:52 +0200

    first commit

---
+++ Java.js
@@ -0,0 +1,6 @@
+/*global console, alert, prompt*/
+var myString = "i love programming languages",
+    
+    mySplit = myString.concat(" vrey much").toUpperCase().split(4, 21);
+
+console.log(mySplit)
